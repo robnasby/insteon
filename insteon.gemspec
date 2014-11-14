4a79f2245da55164a5622a66cd2bc08d92d277d7
Rob Nasby <rob@nasbys.com>
2014-11-13 23:42:55 -0600

    Connect to serial port when initializing PLM.

--- insteon.gemspec
+++ insteon.gemspec
@@ -21,5 +21,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'serialport'
+
   spec.add_development_dependency 'rake'
 end
--- lib/insteon/plm.rb
+++ lib/insteon/plm.rb
@@ -1,6 +1,22 @@
+require 'serialport'
+
 module Insteon
 
   class Plm
+
+    def initialize(port)
+
+    	params = {
+    		"baud"      => 19200,
+    		"data_bits" => 8,
+    		"stop_bits" => 1,
+    		"parity"    => SerialPort::NONE
+    	}
+
+      @serial_port = SerialPort.new(port, params)
+      @serial_port.read_timeout = 250
+
+    end
 
   end
 
